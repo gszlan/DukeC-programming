c0d46dab57351ff5f9d5ed8ad8a3e49f8d926816
Coursera Learner <nobody@nowhere.nul>
2019-07-29 22:48:56 +0000

    06 assignement done

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,19 +16,63 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+struct rect_t {
+    int x,y,width,height;
+};
+typedef struct rect_t rectangle;
 
 rectangle canonicalize(rectangle r) {
-  //WRITE THIS FUNCTION
+  if (r.width < 0) {
+      r.x += r.width;
+      r.width *= -1;
+  }
+  if (r.height < 0) {
+      r.y += r.height;
+      r.height *= -1;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
-  //WRITE THIS FUNCTION
-  return r1;
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  rectangle rec = {0,0,0,0}; 
+
+  if (r1.x < r2.x) {
+      if (r1.x + r1.width < r2.x) {
+          return rec;
+      }
+  } else {
+      if (r2.x + r2.width < r1.x) {
+          return rec;
+      }
+  }
+
+  if (r1.y < r2.y) {
+      if (r1.y + r1.height < r2.y) {
+          return rec;
+      }
+  } else {
+      if (r2.y + r2.height < r1.y) {
+          return rec;
+      }
+  }
+  
+  //printf("mam r1.x = %d, r1.y = %d, r2.x = %d, r2.y = %d \n",r1.x, r1.y, r2.x, r2.y );
+  int rightTopCornerX = min(r1.x + r1.width, r2.x + r2.width);
+  int rightTopCornerY = min(r1.y + r1.height, r2.y + r2.height);
+
+  rec.x = max(r1.x, r2.x);
+  rec.y = max(r1.y, r2.y);
+  //printf("mam r1x %d, r2x %d recx %d\n",r1.x, r2.x, rec.x);
+  rec.width = max(0, rightTopCornerX - rec.x);
+  rec.height = max(0, rightTopCornerY - rec.y);
+
+  return rec;
 }
 
 //You should not need to modify any code below this line
 void printRectangle(rectangle r) {
+
   r = canonicalize(r);
   if (r.width == 0 && r.height == 0) {
     printf("<empty>\n");
