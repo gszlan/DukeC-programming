a42307bf78edc7b1241da247d64191f709f5edc8
Coursera Learner <nobody@nowhere.nul>
2019-08-11 19:46:32 +0000

    done v8

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -15,15 +15,15 @@ void assert_card_valid(card_t c) {
 const char * ranking_to_string(hand_ranking_t r) {
 
     switch(r) {
-        case(STRAIGHT_FLUSH): return "STRAIGHT FLUSH"; break;
-        case(FOUR_OF_A_KIND): return "FOUR OF KIND"; break;
-        case(FULL_HOUSE): return "FULL HOUSE"; break;
+        case(STRAIGHT_FLUSH): return "STRAIGHT_FLUSH"; break;
+        case(FOUR_OF_A_KIND): return "FOUR_OF_KIND"; break;
+        case(FULL_HOUSE): return "FULL_HOUSE"; break;
         case(FLUSH): return "FLUSH"; break;
         case(STRAIGHT): return "STRAIGHT"; break;
-        case(THREE_OF_A_KIND): return "THREE OF KIND"; break;
-        case(TWO_PAIR): return "TWO PAIR"; break;
+        case(THREE_OF_A_KIND): return "THREE_OF_KIND"; break;
+        case(TWO_PAIR): return "TWO_PAIR"; break;
         case(PAIR): return "PAIR"; break;
-        case(NOTHING): return "NOTHNING"; break;
+        case(NOTHING): return "NOTHING"; break;
     }
     return ""; 
 }
