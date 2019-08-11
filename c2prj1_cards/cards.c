984a22fd350fd6348d266652843c4ffa57708f21
Coursera Learner <nobody@nowhere.nul>
2019-08-11 19:33:00 +0000

    done v3

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -92,6 +92,6 @@ card_t card_from_letters(char value_let, char suit_let) {
 card_t card_from_num(unsigned c) {
     card_t temp;
     temp.suit = c / 13;
-    temp.value = (c % 13) + 1; 
+    temp.value = (c % 13) + 2; 
     return temp;
 }
