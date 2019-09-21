f58199f3ad6428e5c9f4a55fbc78c318fdebcf7d
Coursera Learner <nobody@nowhere.nul>
2019-09-21 19:55:23 +0000

    done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -4,16 +4,39 @@
 #include "deck.h"
 void print_hand(deck_t * hand){
 
+    for(int i = 0; i < hand->n_cards; i++) {
+        print_card(*(hand->cards[i]));
+        printf(",");
+    }
+    printf("\n");
 }
 
 int deck_contains(deck_t * d, card_t c) {
+    card_t tmp;
+    for(int i = 0; i < d->n_cards; i++) {
+        tmp = *(d->cards[i]);
+        if (tmp.value == c.value && tmp.suit == c.suit)
+            return 1;
+    }
   return 0;
 }
 
 void shuffle(deck_t * d){
-
+    
+    int random_number;
+    card_t *tmp;
+    size_t count = d->n_cards;
+    for(int i = 0; i < count ; i++) {
+        random_number = random() % count;
+        tmp = d->cards[i];
+        d->cards[i] = d->cards[random_number];
+        d->cards[random_number] = tmp;
+    }
 }
 
 void assert_full_deck(deck_t * d) {
-
+    
+    for (int i = 0; i < 52; i++) {
+        assert(deck_contains(d, card_from_num(i)));
+    }
 }
