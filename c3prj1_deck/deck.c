dce92d38ec4e01ef1758d9ac9a5d50a03caf4e15
Grzesiek <gszlan@tlen.pl>
2019-10-19 23:02:56 +0200

    done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,6 +2,62 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
+
+void add_card_to(deck_t * deck, card_t c) {
+
+  deck->n_cards++;
+  deck->cards = realloc(deck->cards, deck->n_cards);
+  deck->cards[deck->n_cards - 1] = malloc(sizeof(c));
+  *deck->cards[deck->n_cards - 1] = c;
+
+}
+
+card_t * add_empty_card(deck_t * deck) {
+
+  card_t *empty = malloc(sizeof(* empty));
+  empty->value = 0;
+  empty->suit = 0;
+  add_card_to(deck, *empty);
+  return empty;
+
+}
+
+deck_t * make_deck_exclude(deck_t * excluded_cards) {
+
+    deck_t * deck = malloc(sizeof(*deck));
+    
+    for (int i = 0; i < 52; i++) {
+        if (!deck_contains(excluded_cards, card_from_num(i))) {
+            add_card_to(deck, card_from_num(i));
+        }
+    }
+
+    return deck;
+}
+
+deck_t * build_remaining_deck(deck_t **hands, size_t n_hands) {
+
+    deck_t * excluded_cards = malloc(sizeof(*excluded_cards));
+
+    for (int i = 0; i < n_hands; i++) {
+        for (int j = 0; j < hands[i]->n_cards; j++) {
+            add_card_to(excluded_cards, *(hands[i]->cards[j]));
+        }
+    }
+
+    return make_deck_exclude(excluded_cards);
+}
+
+void free_deck(deck_t * deck) {
+
+    for (int i = 0; i < deck->n_cards; i++) {
+        free(deck->cards[i]);
+    }
+    free(deck->cards);
+    free(deck);
+    
+}
+
 void print_hand(deck_t * hand){
 
     for(int i = 0; i < hand->n_cards; i++) {
